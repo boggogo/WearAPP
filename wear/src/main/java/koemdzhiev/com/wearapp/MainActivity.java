1596d41313f57eee4fb7ee232e47bdf335ea963c
boggogo <koemdjiev@gmail.com>
2016-05-28 22:37:52 +0100

    - Start listening for accelerometer when the activity is started

--- wear/src/main/java/koemdzhiev/com/wearapp/MainActivity.java
+++ wear/src/main/java/koemdzhiev/com/wearapp/MainActivity.java
@@ -1,20 +1,34 @@
 package koemdzhiev.com.wearapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
     private String TAG = MainActivity.class.getSimpleName();
     private TextView mTextView;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private double[] gravity = new double[3];
+    private double[] linear_acceleration = new double[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -28,5 +42,46 @@ public class MainActivity extends Activity {
                 });
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this,mSensor,1800);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this,mSensor);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+// In this example, alpha is calculated as t / (t + dT),
+        // where t is the low-pass filter's time-constant and
+        // dT is the event delivery rate.
+
+        float alpha = (float) 0.8;
+
+        // Isolate the force of gravity with the low-pass filter.
+
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+        linear_acceleration[0] = event.values[0] - gravity[0];
+        linear_acceleration[1] = event.values[1] - gravity[1];
+        linear_acceleration[2] = event.values[2] - gravity[2];
+
+        Log.d(TAG,"X: " + Math.round( linear_acceleration[0])+ " Y: " +
+                Math.round(linear_acceleration[1]) + " Z: " +
+                Math.round(linear_acceleration[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
